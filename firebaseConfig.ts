199e934fedf6b198ef45555d4100548b266b42a6
Pastaroom Mapo <pastaroom.mapo@gmail.com>
2020-12-24 16:37:50 +0900

    firebase set

---
+++ firebaseConfig.ts
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyCYMuOcqBUFYk_k8T_NkOWwtQRWHukvmRA',
+  authDomain: 'vue-type.firebaseapp.com',
+  databaseURL: 'https://vue-type-default-rtdb.firebaseio.com',
+  projectId: 'vue-type',
+  storageBucket: 'vue-type.appspot.com',
+  messagingSenderId: '408527236334',
+  appId: '1:408527236334:web:b090b67c31a83ac7fd97ff'
+}
