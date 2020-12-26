76cc821e07c881cdafa022c5194b7fe869538f34
Pastaroom Mapo <pastaroom.mapo@gmail.com>
2020-12-26 20:58:08 +0900

    functions 2

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,4 @@
+import * as functions from 'firebase-functions'
 import userCollection, { User } from './models/users'
 
 const test = async () => {
@@ -9,5 +10,19 @@ const test = async () => {
 }
 
 test()
-
 console.log('functions start')
+
+export const userCreated = functions.auth.user().onCreate(user => {
+  console.log(user)
+  const { uid, email, providerData, displayName, photoURL } = user
+  const ref = userCollection.doc(uid)
+  if (!email) throw Error(`${email} is empty`)
+
+  return ref.set(new User(email, providerData.map(p => p.providerId), displayName, photoURL))
+})
+
+export const userDeleted = functions.auth.user().onDelete(user => {
+  console.log(user)
+  const ref = userCollection.doc(user.uid)
+  return ref.delete()
+})
--- functions/src/models/users.ts
+++ functions/src/models/users.ts
@@ -1,12 +1,17 @@
 import { db } from '../plugins/firebase'
 import firestore from '@google-cloud/firestore'
+import crypto from 'crypto'
 
 export class User {
   constructor (
-    readonly name: string,
+    readonly email: string,
+    readonly providerData?: string[],
+    readonly displayName?: string,
+    readonly photoURL?: string,
+    readonly actions?: string[],
     readonly createdAt?: Date,
     readonly updatedAt?: Date
-  ) {}
+  ) { }
 }
 
 export const converter: firestore.FirestoreDataConverter<User> = {
@@ -19,16 +24,28 @@ export const converter: firestore.FirestoreDataConverter<User> = {
       // 아래 한 줄로 요약
       return Object.assign(model, { updatedAt: firestore.FieldValue.serverTimestamp() })
     }
+    const hash = crypto.createHash('md5').update(model.email).digest('hex')
     return {
-      name: model.name + ' a',
+      email: model.email,
+      providerId: model.providerData || [],
+      displayName: model.displayName || '',
+      photoURL: model.photoURL || `http://www.gravatar.com/avatar/${hash}.jpg`,
+      actions: model.actions || [],
       createdAt: model.createdAt || firestore.FieldValue.serverTimestamp(),
       updatedAt: model.updatedAt || firestore.FieldValue.serverTimestamp()
     }
   },
   fromFirestore: (snapshot: firestore.QueryDocumentSnapshot) => {
     const data = snapshot.data()
+
+    let photoURL = data.photoURL || ''
+    if (photoURL.include('gravatar')) photoURL += '?d=identicon'
     return new User(
-      data.name + ' b',
+      data.email,
+      data.providerData,
+      data.displayName,
+      photoURL,
+      data.actions,
       data.createdAt.toDate(),
       data.updatedAt.toDate()
     )
